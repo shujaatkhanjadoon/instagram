c12379dcebe6d5e37333eca74d50634e44742d9b
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2023-11-20 10:47:44 +0000

    test

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -11,6 +11,11 @@ const AuthLayout = ()=> {
             <section className="flex flex-1 justify-center items-center py-10 flex-col">
                 <Outlet />
             </section>
+            <img
+            src='/assets/images/side-img.svg'
+            alt='bg-image'
+            className="hidden md:block object-cover h-screen w-1/2 bg-no-repeat"
+             />
             </>
         )
         }
