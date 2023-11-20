8d90a6212b35e3600de9a22295f6b4c268bccab2
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2023-11-20 12:19:47 +0000

    api

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,38 +1,59 @@
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod"
 
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage,
   } from "@/components/ui/form"
   import { Input } from "@/components/ui/input"
 
 
 import { Button } from "@/components/ui/button"
-import { useForm } from "react-hook-form/dist";
+import { useForm } from "react-hook-form";
+import { SignupValidation } from "@/lib/validation";
+import Loader from "@/components/shared/Loader";
+import { Link } from "react-router-dom";
 
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
-  });
+
 
 const SignupForm = ()=> {
 
-
-      const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+     const isLoading = false;
+      const form = useForm<z.infer<typeof SignupValidation>>({
+        resolver: zodResolver(SignupValidation),
         defaultValues: {
-          username: "",
+          name: '',
+          username: '',
+          email: '',
+          password: '',
         },
       })
      
       // 2. Define a submit handler.
-      function onSubmit(values: z.infer<typeof formSchema>) {
-        
-        console.log(values)
+     async function onSubmit(values: z.infer<typeof SignupValidation>) {
+        // const newUser = await createUserAccount(values);
       }
     
 
     return(
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className="sm:w-420 flex-col flex-center">
+            <img src='/assets/images/logo.svg' alt='logo' />
+            <h2 className="h3-bold md:h2-bold pt-5 sm:tp-12">Create a new account</h2>
+            <p className="small-medium text-light-3 md:base-regular mt-2">To use Instagram, please enter your details.</p>
+          
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 mt-4 w-full md:px-4">
+                <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                    <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                        <Input type='text' className="shad-input" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                    </FormItem>
+                )}
+                />
                 <FormField
                 control={form.control}
                 name="username"
@@ -40,17 +61,54 @@ const SignupForm = ()=> {
                     <FormItem>
                     <FormLabel>Username</FormLabel>
                     <FormControl>
-                        <Input placeholder="shadcn" {...field} />
+                        <Input type='text' className="shad-input" {...field} />
                     </FormControl>
-                    <FormDescription>
-                        This is your public display name.
-                    </FormDescription>
                     <FormMessage />
                     </FormItem>
                 )}
                 />
-                <Button type="submit">Submit</Button>
+                <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                    <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                        <Input type='email' className="shad-input" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                    </FormItem>
+                )}
+                />
+                <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                    <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                        <Input type='password' className="shad-input" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                    </FormItem>
+                )}
+                />
+                <Button type="submit" className="shad-button_primary">
+                  {isLoading ? (
+                    <div className="flex-center gap-2">
+                      <Loader /> Loading ...
+                    </div>
+                  ) : 'Sign Up'}
+                  </Button>
+                  <p className="text-small-regular text-center text-light-2 mt-2">
+                    Already have an account? 
+                    <Link to='/sign-in' className="text-primary-500 text-small-semibold ml-1">
+                      Log In
+                    </Link>
+
+                  </p>
             </form>
+            </div>
          </Form>
     )
 }
